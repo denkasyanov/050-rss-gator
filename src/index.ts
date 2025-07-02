e211f0787ca23875d54c26be3595cdf3052f36de
Den Kasyanov <den@den.ai>
2025-07-03 02:10:28 +0400

    Implement command registry and command execution in main function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
-import { readConfig, setUser } from "./config.js";
+import {
+  CommandRegistry,
+  registerCommand,
+  runCommand,
+} from "./commands/index.js";
+import { handlerLogin } from "./commands/users.js";
 
 function main() {
-  setUser("Den");
-  const config = readConfig();
-  console.log(config);
+  const registry: CommandRegistry = {};
+  registerCommand(registry, "login", handlerLogin);
+
+  const args = process.argv.slice(2);
+  if (args.length === 0) {
+    console.log("Usage: gator <command> [args...]");
+    process.exit(1);
+  }
+  const cmdName = args[0];
+  const cmdArgs = args.slice(1);
+  try {
+    runCommand(registry, cmdName!, ...cmdArgs);
+  } catch (error) {
+    console.log((error as Error).message);
+    process.exit(1);
+  }
 }
 
 main();
